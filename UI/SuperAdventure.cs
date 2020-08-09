7e97b436b2f62e387d18422eae1aba58abe670a4
Artem <artemesin2016@gmail.com>
2020-08-09 16:17:52 +0700

    Added labels for main stats and a test button

---
+++ UI/SuperAdventure.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UI
+{
+    public partial class SuperAdventure : Form
+    {
+        public SuperAdventure()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            lblExperience.Text = "999";
+            lblGold.Text = "-2222";
+            lblHitPoints.Text = "24";
+            lblLevel.Text = "9";
+        }
+    }
+}
