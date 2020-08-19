1c0ea0dca1709baa1b639445ff36c30bc869f69b
Artem <artemesin2016@gmail.com>
2020-08-19 19:47:36 +0700

    Added game object classes in engine; added creation of the player entity when UI form are created; deleted the "test" button from the main window

--- UI/SuperAdventure.cs
+++ UI/SuperAdventure.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using Engine;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Media;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,17 +14,25 @@ namespace UI
 {
     public partial class SuperAdventure : Form
     {
+
+        private Player _player;
+
         public SuperAdventure()
         {
             InitializeComponent();
-        }
 
-        private void btnTest_Click(object sender, EventArgs e)
-        {
-            lblExperience.Text = "999";
-            lblGold.Text = "-2222";
-            lblHitPoints.Text = "24";
-            lblLevel.Text = "9";
+            _player = new Player();
+
+            _player.MaximumHitPoints = 10;
+            _player.CurrentHitPoints = 10;
+            _player.ExperiencePoints = 0;
+            _player.Level = 1;
+            _player.Gold = 20;
+
+            lblHitPoints.Text = _player.CurrentHitPoints.ToString();
+            lblGold.Text = _player.Gold.ToString();
+            lblExperience.Text = _player.ExperiencePoints.ToString();
+            lblLevel.Text = _player.Level.ToString();
         }
     }
 }
